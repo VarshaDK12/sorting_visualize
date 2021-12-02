021fb4c74810b8a0d7cfe633c203f8af96ac8ea7
VarshaDK12 <varshadharmapal@gmail.com>
2021-12-02 08:16:56 +0530

    sort algo project

---
+++ sort-visualizer/src/components/molecules/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      
+
+      
+    </footer>
+  );
+};
+
+export default Footer;
